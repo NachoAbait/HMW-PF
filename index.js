0a41b7f7d41b1f73e31edbb00d82972e78a1a2fb
Andres <andrescantlon@gmail.com>
2022-11-20 14:07:47 -0300

    Andy Homework

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
+    if (numero % 2 === 0) {
+     let binario = numero.toString(2);
+     return binario;
+   } else {
+     let hexadecimal = numero.toString(16);
+     return hexadecimal;
+   }
 }
